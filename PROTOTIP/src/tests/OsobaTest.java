17477a1e97cdbab5339b6f945e78d79c0502aa3f
MarinaVojnovic <marina.vojnovic@gmail.com>
2018-07-15 21:59:09 +0200

    Marina dodala test za setIme kod osobe

---
+++ PROTOTIP/src/tests/OsobaTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Osoba;
+
+public class OsobaTest {
+	Osoba o;
+
+	@Test
+	public void testSetIme() {
+		o=new Osoba();
+		o.setIme("Milica");
+		assertEquals(o.getIme(),"Milica");
+		
+	}
+}
